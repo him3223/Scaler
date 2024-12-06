58fc0c4a9b4310d34cc58a9cb3c0eba2b0b1bd2e
him3223 <Himanshu.gupta3223@gmail.com>
2024-12-06 23:23:57 +0530

    Sum of all Submatrices
    Sum of all Submatrices Final Code and practice

---
+++ FinalCode/MODULE - 2/DSA Arrays 2 Two Dimensional/Sum of all Submatrices/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static void main(String[] args) {
+        // Input matrix
+        int[][] A = {
+            {3, 2, 6},
+            {4, 5, 9},
+            {8, 7, 2}
+        };
+        System.out.println("Sum of all submatrices: " + calculateSubmatrixSum(A));
+    }
+    
+    public static int calculateSubmatrixSum(int[][] A) {
+        int ans=0;
+        int len = A.length;
+        for (int a=0;a<len ;a++ ) {
+            for (int b=0;b<len ;b++ ) {
+                ans+=(A[a][b]*(a+1)*(b+1)*(len-a)*(len-b));
+            }
+        }
+        return ans;
+    }
+}
