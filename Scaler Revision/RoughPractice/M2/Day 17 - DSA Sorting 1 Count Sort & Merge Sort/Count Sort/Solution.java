19d01b4506fe104f574445fef883f5e1ea9f92a7
him3223 <Himanshu.gupta3223@gmail.com>
2025-05-16 23:16:16 +0530

    M1
    commit regards sorts

---
+++ Scaler Revision/RoughPractice/M2/Day 17 - DSA Sorting 1 Count Sort & Merge Sort/Count Sort/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Solution {
+    public static int[] solve(int[] A) {
+        int len = A.length;
+        int max = A[0];
+        int min = A[0];
+        int num = 0;
+        int range = 0;
+        int index = 0;
+        for (int a=0; a<len; a++) {
+            num = A[a];
+            min=Math.min(min,num);
+            max=Math.max(max,num);
+        }
+        range = max-min+1;
+        int[] count = new int[range];
+        for (int a=0; a<len; a++) {
+            num = A[a];
+            count[num-min]++;
+        }
+        for (int a=0; a<range; a++) {
+            while (count[a]-- > 0) {
+                A[index++]=a+min;
+            }
+        }
+        return A;
+    }
+    public static void main(String[] args) {
+        int[] A = {4, 2, 1, 3};
+        System.out.println(Arrays.toString(solve(A)));
+    }
+}
